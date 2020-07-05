282933a6e0c1a9720d522123ca5a498b1f707cc1
wangronin <wangronin@gmail.com>
2020-07-05 21:03:49 +0200

    change a label in 'ERT_box.R' mean --> PAR-1

--- inst/shiny-server/ui/ERT_box.R
+++ inst/shiny-server/ui/ERT_box.R
@@ -30,6 +30,7 @@ ERT_box <- function(width = 12, collapsible = T, collapsed = T) {
                   placement = "auto"
                 )
             ),
+          
           textInput('ERTPlot.Max',
                     label = F_MAX_LABEL,
                     value = ''),
@@ -39,8 +40,16 @@ ERT_box <- function(width = 12, collapsible = T, collapsed = T) {
                         value = T),
 
           checkboxInput('ERTPlot.show.mean',
-                        label = 'Show/hide mean',
-                        value = F),
+                        label = 'Show/hide PAR-1',
+                        value = F) %>% 
+            shinyInput_label_embed(
+              custom_icon() %>%
+                bs_embed_popover(
+                  title = "Penalized Average Runtime", content = "PAR-1 score is ther average of running time values, 
+                    where non-successful runs are counted as evaluation budget B.", 
+                  placement = "auto"
+                )
+            ),
 
           checkboxInput('ERTPlot.show.CI',
                         label = 'Show/hide mean +/- sd',
